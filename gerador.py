43887dbfdc52fa080d9e53d6100c719bd16bc9fc
DaniloTakata <daniloissamu@gmail.com>
2023-07-12 14:27:32 -0300

    code conclued

---
+++ gerador.py
@@ -0,0 +1,11 @@
+from random import choice
+import string
+
+password = ""
+passwd_lenght = int(input("Input the password's lenght: "))
+characters = string.ascii_letters + string.digits + string.punctuation
+
+for element in range(passwd_lenght):
+    password += choice(characters)
+
+print(f"New password here: {password}")
